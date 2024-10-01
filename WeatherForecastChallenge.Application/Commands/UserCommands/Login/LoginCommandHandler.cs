3af946ce60dae9fa99a86f59541461dba88ab31e
Vitu <vitorcsharp@hotmail.com>
2024-10-01 18:13:22 -0300

    refactor: Creating my CQRS standard in my application layer to perform user registration and user login along with our jwt token. This removes responsibility from the API layer

---
+++ WeatherForecastChallenge.Application/Commands/UserCommands/Login/LoginCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Threading;
+using System.Threading.Tasks;
+using WeatherForecastChallenge.Application.Commands.UserCommands.Login;
+using WeatherForecastChallenge.Core.Response;
+using WeatherForecastChallenge.Infrastructure.Auth;
+
+public class UserLoginCommandHandler : IRequestHandler<LoginCommand, UserLoginResponse>
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly ITokenServices _tokenServices;
+
+    public UserLoginCommandHandler(SignInManager<IdentityUser> signInManager, ITokenServices tokenServices)
+    {
+        _signInManager = signInManager;
+        _tokenServices = tokenServices;
+    }
+
+    public async Task<UserLoginResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var result = await _signInManager.PasswordSignInAsync(request.Email, request.Password, false, true);
+
+        if (result.Succeeded)
+        {
+            return await _tokenServices.GenerateJwt(request.Email);
+        }
+
+        throw new Exception("Login falhou: usuário ou senha incorretos.");
+    }
+}
