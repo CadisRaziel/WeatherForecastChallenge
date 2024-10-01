f5fb1f851589752064ff093abb8a4b6379737152
Vitu <vitorcsharp@hotmail.com>
2024-10-01 18:10:56 -0300

    refactor: Removing appSettings and token generation from my api layer and placing it in my infra layer, thus creating an interface for my token generation to improve the code

---
+++ WeatherForecastChallenge.Infrastructure/Extension/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherForecastChallenge.Infrastructure.Extension
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public int ExpirationHours { get; set; }
+        public string Issuer { get; set; }
+        public string ValidOn { get; set; }
+    }
+}
