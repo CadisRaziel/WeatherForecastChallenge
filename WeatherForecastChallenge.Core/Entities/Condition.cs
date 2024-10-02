8f061362f699ed53b9b07b80d72f75658a5ee36f
Vitu <vitorcsharp@hotmail.com>
2024-10-01 22:19:36 -0300

    feat: Creating a model of our weather forecast api to take data such as city - region - country - temperature - wind speed - humidity - condition and condition icon. We've also created a response to serve as a template for our response json.

---
+++ WeatherForecastChallenge.Core/Entities/Condition.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherForecastChallenge.Core.Entities
+{
+    public class Condition
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+    }
+}
---
+++ WeatherForecastChallenge.Core/Response/WeatherApiResponse.cs
@@ -0,0 +1,10 @@
+﻿using WeatherForecastChallenge.Core.Entities;
+
+namespace WeatherForecastChallenge.Core.Response
+{
+    public class WeatherApiResponse
+    {
+        public Location Location { get; set; }
+        public Current Current { get; set; }
+    }
+}
