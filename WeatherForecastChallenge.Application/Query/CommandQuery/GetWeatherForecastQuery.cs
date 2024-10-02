5638dd3d76f4cda8f95c8cbdb34baa94b606ce9d
Vitu <vitorcsharp@hotmail.com>
2024-10-01 22:21:04 -0300

    feat: Creating the commands queries to perform the cqrs pattern to take the city that the user types in and fetch it from the WeatherAPI via http

---
+++ WeatherForecastChallenge.Application/Query/CommandQuery/GetWeatherForecastQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using WeatherForecastChallenge.Core.Response;
+
+namespace WeatherForecastChallenge.Application.Query.CommandQuery
+{
+    public class GetWeatherForecastQuery : IRequest<WeatherApiResponse>
+    {
+        public string City { get; }
+
+        public GetWeatherForecastQuery(string city)
+        {
+            City = city;
+        }
+    }
+}
---
+++ WeatherForecastChallenge.Application/Query/CommandQueryHandler/GetWeatherForecastQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using WeatherForecastChallenge.Application.Query.CommandQuery;
+using WeatherForecastChallenge.Core.Response;
+
+public class GetWeatherForecastQueryHandler : IRequestHandler<GetWeatherForecastQuery, WeatherApiResponse>
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IConfiguration _configuration;
+
+    public GetWeatherForecastQueryHandler(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        _httpClientFactory = httpClientFactory;
+        _configuration = configuration;
+    }
+
+    public async Task<WeatherApiResponse> Handle(GetWeatherForecastQuery request, CancellationToken cancellationToken)
+    {
+        var apiKey = _configuration["WeatherApi:ApiKey"];
+        var city = request.City;
+       
+        var httpClient = _httpClientFactory.CreateClient("WeatherAPI");
+       
+        var requestUri = $"current.json?key={apiKey}&q={city}&aqi=no"; 
+        
+        var response = await httpClient.GetAsync(requestUri);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception("Erro ao chamar a API de previsão do tempo: " + response.ReasonPhrase);
+        }
+
+        var content = await response.Content.ReadAsStringAsync();
+        var weatherApiResponse = JsonConvert.DeserializeObject<WeatherApiResponse>(content);
+
+        return weatherApiResponse;
+    }
+}
