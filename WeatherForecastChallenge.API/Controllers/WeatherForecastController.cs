ce6510ee1d1787eacc9c6cc80154497749a57b86
Vitu <vitorcsharp@hotmail.com>
2024-10-03 20:48:17 -0300

    feat: creating our controller and documenting the 5-day forecast api and adding authorization to it

--- WeatherForecastChallenge.API/Controllers/WeatherForecastController.cs
+++ WeatherForecastChallenge.API/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ public class WeatherForecastController : ControllerBase
     }
 
     /// <summary>
-    /// Weather forecast api
+    /// current day's weather forecast
     /// </summary>
     /// <param name="city"></param>
     /// <returns>Weather forecast api that fetches data from WeatherAPI and returns location - temperature - humidity - weather condition - wind speed</returns>
@@ -30,5 +30,26 @@ public class WeatherForecastController : ControllerBase
         var result = await _mediator.Send(query);
 
         return Ok(result);
+    }
+
+
+    /// <summary>
+    /// weather forecast for the next 5 days
+    /// </summary>
+    /// <param name="city"></param>
+    /// <returns>returns an object with a list of the weather forecast 5 days ahead</returns>
+    [HttpGet("fivedays/{city}")]
+    [ProducesResponseType(typeof(WeatherApiFiveDayResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetWeatherForecastFiveDay(string city)
+    {
+        var query = new GetWeatherForecastFiveDayQuery(city);
+        var result = await _mediator.Send(query); 
+
+        if (result == null)
+        {
+            return NotFound(new { Message = $"Previsão do tempo para {city} não encontrada." });
+        }
+
+        return Ok(result); 
     }
 }
